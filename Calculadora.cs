e89504a733919a30c3131b918bb51921e38135d7
Fábio Cordeiro <fabio@pucminas.br>
2023-05-11 20:30:58 -0300

    Exemplo 2 - Delegates

---
+++ Calculadora.cs
@@ -0,0 +1,12 @@
+class Calculadora
+{
+    public static int Soma(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtracao(int a, int b)
+    {
+        return a - b;
+    }
+}
--- Questao1.cs
+++ Questao1.cs
@@ -1,7 +1,26 @@
 class Questao1
 {
+    delegate int CalcularDelegate(int a, int b);
     public static void Executar(){
         Console.WriteLine("Atividade 1 -----------: ");
+
+        // Cria uma instância do delegate CalcularDelegate
+        CalcularDelegate calcular;
+
+        // Atribui o método Soma ao delegate
+        calcular = Calculadora.Soma;
+        int resultadoSoma = calcular(5, 3);
+        Console.WriteLine("Resultado da soma: " + resultadoSoma);
+
+        // Atribui o método Subtracao ao delegate
+        calcular = Calculadora.Subtracao;
+        int resultadoSubtracao = calcular(10, 4);
+        Console.WriteLine("Resultado da subtração: " + resultadoSubtracao);
+
+        Console.ReadLine();
+
+
+
     }
     
 }
